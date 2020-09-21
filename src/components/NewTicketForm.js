0a9bdf046087193bf543e8e9baffcff010814ef4
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-09-21 11:55:16 -0700

    add full CRUD functionality using firebase
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: aesadin <aesadin@gmail.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-import Moment from 'moment';
+import { useFirestore } from 'react-redux-firebase';
 
 function NewTicketForm(props){
 
-  function handleNewTicketFormSubmission(event) {
+  const firestore = useFirestore();
+
+  function addTicketToFirestore(event) {
     event.preventDefault();
-    props.onNewTicketCreation({
-      names: event.target.names.value,
-      location: event.target.location.value,
-      issue: event.target.issue.value,
-      id: v4(),
-      timeOpen: new Moment(),
-      formattedWaitTime: new Moment().fromNow(true)
-    });
+    props.onNewTicketCreation();
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value, 
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    );
   }
 
   return (
     <React.Fragment>
       <ReusableForm 
-        formSubmissionHandler={handleNewTicketFormSubmission}
+        formSubmissionHandler={addTicketToFirestore}
         buttonText="Help!" />
     </React.Fragment>
   );
@@ -32,3 +34,8 @@ NewTicketForm.propTypes = {
 };
 
 export default NewTicketForm;
+
+// Is this still shared state, or are we now updating local state? If it's shared where are we accessing the firestoreReducer/state slice?
+// Why do we need redux if we are using a database now? Are we replacing the store?
+// Why do we still need reducers/what are they doing? Do we need reducers and a store and a database?
+// Do we need actions at all? We only use toggleForm now; could we use a hook to update that state instead?
